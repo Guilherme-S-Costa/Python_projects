7e736732f83c159d6a56743f73d4e779e7042feb
Guilherme Silva Costa <guilhermesilvac.gc@gmail.com>
2023-04-02 13:29:48 -0300

    Conclusão projeto 2 semana

--- project2/news/models.py
+++ project2/news/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-# O modelo representa uma entidade (TABELA) dentro do banco de dados.
 
 class Postagens(models.Model):
     titulo = models.CharField(max_length=200)
